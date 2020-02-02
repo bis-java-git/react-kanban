8e6ad9242203de653717859ac1b9fd66c2269f43
hemant-2 <hemant@softwarehouse.co>
2020-02-02 22:47:05 +0000

    Added List of tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,38 @@
-import React, { Component } from "react";
-import Note from './Note.jsx';
+
+import React from "react";
+import uuid from 'uuid';
+
+const uuidv4 = require('uuid/v4');
+
 export default class app extends React.Component {
     render() {
-        return <Note />;
+        const notes = [
+            {
+                id: uuidv4(),
+                task: 'Learn Webpack'
+            },
+        {
+            id: uuidv4(),
+            task: 'Learn React'
+           },
+
+        {
+            id: uuidv4(),
+            task: 'Hemant Task'
+        }
+        ];
+        
+        console.log(notes);
+
+        return (
+          
+            <div>
+                <ul>{ notes.map( note =>  
+                    <li key={note.id}> {note.task}</li>
+                          
+                )}
+                </ul>
+            </div>
+        );
     }
 }
