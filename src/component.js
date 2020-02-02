2605fae957e126c1efa58ef121bfb85b0a036e0a
hemant-2 <hemant@softwarehouse.co>
2020-02-02 20:28:18 +0000

    Modified gitignore and visual studio auto check

--- src/component.js
+++ src/component.js
@@ -1,5 +1,5 @@
 module.exports = function () {
 	var element = document.createElement('h1');
-	element.innerHTML = 'Hello Hemant Kanban';
+	element.innerHTML = 'Hello Hemant First React Kanban';
 	return element;
 }
